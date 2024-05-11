af430278c2b44579a5763fe3babc82d8343f9307
Satya Uttami <satyautta@gmail.com>
2024-05-11 22:47:23 +0800

    views

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
+</head>
+
+<body>
+
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">contact</a>
+    <h1>Halaman Blog</h1>
+    <article>
+        <h3>Judul 1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ea incidunt dignissimos accusantium
+            praesentium officiis, quidem aspernatur fugiat amet ipsam rem inventore rerum ex fugit voluptas illum
+            accusamus deserunt suscipit?</p>
+    </article>
+    <article>
+        <h3>Judul 2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ea incidunt dignissimos accusantium
+            praesentium officiis, quidem aspernatur fugiat amet ipsam rem inventore rerum ex fugit voluptas illum
+            accusamus deserunt suscipit?</p>
+    </article>
+</body>
+
+</html>
